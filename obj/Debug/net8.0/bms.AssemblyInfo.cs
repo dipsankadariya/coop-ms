9844abb2404b635436fc90e6f69a8b3b56ce9bce
Dipsan Kadariya <dipsankadariya99@gmail.com>
2026-01-18 15:58:34 +0545

    Added gitignore

--- obj/Debug/net8.0/bms.AssemblyInfo.cs
+++ obj/Debug/net8.0/bms.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bms")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d21d5cefcfde4a9200ddab6fad95198213651ee7")]
 [assembly: System.Reflection.AssemblyProductAttribute("bms")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bms")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
